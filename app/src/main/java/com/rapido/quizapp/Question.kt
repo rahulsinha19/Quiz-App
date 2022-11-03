287acffd7b24cb999a6f242b07f90633efd088fc
Rahul Sinha <rahul.sinha2k@gmail.com>
2022-11-03 17:23:24 +0530

    Setting up the main screen, the question screen and the data class for questions

---
+++ app/src/main/java/com/rapido/quizapp/Question.kt
@@ -0,0 +1,20 @@
+package com.rapido.quizapp
+
+/*
+defines the parameters for the question like what do i need to know about each question,
+what kind of information it need.
+*/
+
+data class Question(
+    val id : Int, // -> for question number
+    val questions : String, // -> for the question statement
+    val image : Int, // -> for the images in the question
+
+    // for the 4 options in the question
+    val optionOne : String,
+    val optionTwo : String,
+    val optionThree : String,
+    val optionFour : String,
+
+    val correctAnswer : Int
+)
